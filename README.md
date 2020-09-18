d014e0c514f6c4345cfd73aca3d7929333de6890
Frederic Houle <frederic@frederichoule.com>
2020-09-18 17:57:30 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # google-iam-roles
-Manages your user's IAM roles
+Manages your user's custom IAM roles. Useful for Google IAP protected applications. It only shows custom IAM roles, built-in roles are filtered out.
+
+## Installation
+
+It's recommended that you use [Composer](https://getcomposer.org/) to install google-iam-roles.
+
+```bash
+$ composer require attraction/google-iam-roles:^1.0
+```
+
+This will install google-iam-roles and all required dependencies. google-iam-roles requires PHP 7.4 or newer.
+
+## Requirements
+First, activate the Cloud Resource Manager API [here](https://console.developers.google.com/apis/api/cloudresourcemanager.googleapis.com/overview)
+
+Second, make sure your service account has the `Project IAM Admin` role.
+
+Third, this class uses `Application Default Credentials` to access Google Cloud - you can read about it [here](https://cloud.google.com/docs/authentication/production#passing_variable) if you're not familiar.
+
+## Usage
+
+```php
+use Attraction\GoogleIAMRoles;
+
+// Project ID is available in Google Cloud Project Settings - https://console.cloud.google.com/iam-admin/settings
+$projectId = 'YOUR_PROJECT_ID';
+$applicationName = 'A simple name to describe your application';
+
+$iam = new GoogleIAMRoles($applicationName, $projectId);
+
+/*
+    Returns an object in this format:
+    [
+        {
+            "role": "CustomRoleUser",
+            "members": [
+                "someone@john@doe.com"
+            ]
+        }
+    ]
+*/
+$roles = $iam->getProjectRoles();
+
+// Add role(s) to user
+$iam->addRole('john@doe.com','CustomRoleAdmin');
+$iam->addRoles('john@doe.com',['CustomRoleUser','CustomRoleAdmin']);
+
+// Remove role(s) from user
+$iam->removeRole('john@doe.com','CustomRoleAdmin');
+$iam->removeRoles('john@doe.com',['CustomRoleUser','CustomRoleAdmin']);
+
+// Test if user has a specific role
+$iam->hasRole('john@doe.com','CustomRoleAdmin');
+
+// Test if user has ANY of the roles
+$iam->hasRoles('john@doe.com',['CustomRoleAdmin','CustomRoleUser']);
+
+// Test if user has ALL the roles
+$iam->hasRoles('john@doe.com',['CustomRoleAdmin','CustomRoleUser'], true);
+```
---
+++ src/Attraction/class.googleiamroles.php
@@ -0,0 +1,289 @@
+<?php
+
+namespace Attraction;
+
+class GoogleIAMRoles
+{
+
+    private $googleClient, $applicationName, $projectId, $scopes, $guzzleClient, $cacheLayer = [];
+
+    const CRM_ENDPOINT = 'https://cloudresourcemanager.googleapis.com/v1/projects';
+    const SCOPES = ['https://www.googleapis.com/auth/cloud-platform'];
+
+    public function __construct(string $applicationName, string $projectId)
+    {
+
+        $this->applicationName = $applicationName;
+        $this->projectId = $projectId;
+
+        $this->googleClient = new \Google_Client();
+        $this->googleClient->setApplicationName($this->applicationName);
+        $this->googleClient->setScopes(self::SCOPES);
+        $this->googleClient->useApplicationDefaultCredentials();
+
+        $this->guzzleClient = new \GuzzleHttp\Client();
+    }
+
+    private function emptyCache()
+    {
+        $this->cacheLayer = [];
+    }
+
+    private function cache($key, $value = false, $forceUpdate = false)
+    {
+        if ($forceUpdate) {
+            unset($this->cacheLayer[$key]);
+        }
+        return $this->cacheLayer[$key] ?? $this->cacheLayer[$key] = ($value instanceof Closure ? $value() : $value);
+    }
+
+    private function isCustomRole(string $role)
+    {
+        $base = sprintf('projects/%s/roles/', $this->projectId);
+        if (strpos($role, $base) === false) {
+            return false;
+        }
+
+        $segments = explode('/', $role);
+        return $segments[count($segments) - 1];
+    }
+
+    private function hydrateCustomRole(string $role)
+    {
+        return sprintf('projects/%s/roles/%s', $this->projectId, $role);
+    }
+
+    private function getAccessToken()
+    {
+        $this->googleClient->fetchAccessTokenWithAssertion();
+        $access_token = $this->googleClient->getAccessToken()['access_token'] ?? false;
+        return $access_token;
+    }
+
+    public function removeRole(string $emailAddress, string $role)
+    {
+        return $this->removeRoles($emailAddress, [$role]);
+    }
+
+    public function removeRoles(string $emailAddress, array $roles)
+    {
+
+        $customRoles = array_map(function ($role) {
+            return $this->hydrateCustomRole($role);
+        }, $roles);
+
+        $memberToRemove = sprintf('user:%s', $emailAddress);
+
+        $res = $this->cache('getIamPolicy', function () {
+            return $this->crmPost('getIamPolicy');
+        });
+
+        if (!$res['success'] || !isset($res['data']->bindings)) {
+            return false;
+        }
+
+        $iamPolicy = $res['data'];
+
+        foreach ($iamPolicy->bindings as $binding) {
+            if (in_array($binding->role, $customRoles)) {
+                if (in_array($memberToRemove, $binding->members)) {
+                    $binding->members = array_diff($binding->members, [$memberToRemove]);
+                }
+            }
+        }
+
+        return $this->updateIamPolicy($iamPolicy);
+    }
+
+    public function addRole(string $emailAddress, string $role)
+    {
+        return $this->addRoles($emailAddress, [$role]);
+    }
+
+    public function addRoles(string $emailAddress, array $roles)
+    {
+
+        $customRoles = array_map(function ($role) {
+            return $this->hydrateCustomRole($role);
+        }, $roles);
+
+        $memberToAdd = sprintf('user:%s', $emailAddress);
+
+        $res = $this->cache('getIamPolicy', function () {
+            return $this->crmPost('getIamPolicy');
+        });
+
+        if (!$res['success'] || !isset($res['data']->bindings)) {
+            return false;
+        }
+
+        $iamPolicy = $res['data'];
+        $added = [];
+
+        foreach ($iamPolicy->bindings as $binding) {
+            if (in_array($binding->role, $customRoles)) {
+                if (!in_array($memberToAdd, $binding->members)) {
+                    $binding->members[] = $memberToAdd;
+                    $added[] = $binding->role;
+                }
+            }
+        }
+
+        $remainingRoles = array_diff($customRoles, $added);
+
+        foreach ($remainingRoles as $role) {
+            $iamPolicy->bindings[] = (object) [
+                'role' => $role,
+                'members' => [$memberToAdd]
+            ];
+            $added[] = $role;
+        }
+
+        return $this->updateIamPolicy($iamPolicy);
+    }
+
+    private function updateIamPolicy(object $iamPolicy)
+    {
+
+        $res = $this->crmPost('setIamPolicy', [
+            'policy' => $iamPolicy
+        ]);
+
+        if ($res['success']) {
+            $this->emptyCache();
+            return true;
+        }
+
+        return false;
+    }
+
+    public function hasRole(string $emailAddress, string $role)
+    {
+        return $this->hasRoles($emailAddress, [$role]);
+    }
+
+    public function hasRoles(string $emailAddress, array $roles = [], bool $hasAllRoles = false)
+    {
+
+        $emailRolesMap = $this->cache('emailRolesMap', function () {
+            $projectRoles = $this->getProjectRoles();
+            $emailRolesMap = [];
+
+            foreach ($projectRoles as $projectRole) {
+
+                foreach ($projectRole->members as $member) {
+                    isset($emailRolesMap[$member]) ? $emailRolesMap[$member][] = $projectRole->role : $emailRolesMap[$member] = [$projectRole->role];
+                }
+            }
+
+            return $emailRolesMap;
+        });
+
+        if (!array_key_exists($emailAddress, $emailRolesMap)) {
+            return false;
+        }
+
+        $success = 0;
+        $total = 0;
+
+        foreach ($roles as $role) {
+            if (in_array($role, $emailRolesMap[$emailAddress])) {
+                $success++;
+            }
+            $total++;
+        }
+
+        return ($hasAllRoles ? ($success == $total) : ($success > 0));
+    }
+
+    public function getProjectRoles()
+    {
+        return $this->cache('getProjectRoles', function () {
+            return $this->_getProjectRoles();
+        });
+    }
+
+    private function getIamPolicy()
+    {
+
+        $res = $this->cache('getIamPolicy', function () {
+            return $this->crmPost('getIamPolicy');
+        });
+
+        if (!$res['success'] || !isset($res['data']->bindings)) {
+            return false;
+        }
+
+        return $res['data'] ?? false;
+    }
+
+    private function _getProjectRoles()
+    {
+
+        $iamPolicy = $this->getIamPolicy();
+        if (!$iamPolicy) {
+            return false;
+        }
+
+        $bindings = $iamPolicy->bindings ?? [];
+
+        $roles = [];
+
+        foreach ($bindings as $binding) {
+
+            $iamRole = $this->isCustomRole($binding->role ?? '');
+
+            if (!$iamRole) {
+                continue;
+            }
+
+            $role = [
+                'role' => $iamRole,
+                'members' => []
+            ];
+
+            $members = $binding->members ?? [];
+            foreach ($members as $member) {
+                $emailAddress = explode('user:', $member)[1];
+                if (is_null($emailAddress)) {
+                    continue;
+                }
+                $role['members'][] = $emailAddress;
+            }
+
+            $roles[] = $role;
+        }
+
+        return $roles;
+    }
+
+    private function crmPost(string $method, array $body = [])
+    {
+
+        $finalUrl = sprintf(self::CRM_ENDPOINT . '/%s:%s?alt=json', $this->projectId, $method);
+        $accessToken = $this->getAccessToken();
+        if (!$accessToken) {
+            return false;
+        }
+
+        $parameters = [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $accessToken
+            ],
+            'http_errors' => false
+        ];
+
+        if (count($body) > 0) {
+            $parameters['json'] = $body;
+        }
+
+        $res = $this->guzzleClient->request('POST', $finalUrl, $parameters);
+
+        $success = ($res->getStatusCode() == 200);
+
+        return [
+            'success' => $success,
+            'data' => json_decode(($res->getBody()->getContents() ?? '{}'))
+        ];
+    }
+}
